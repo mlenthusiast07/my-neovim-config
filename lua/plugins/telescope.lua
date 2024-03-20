37e0807442f2d022c30c87e78cd322e197418fe8
Aditya Ramesh Ganti <adityaramesh@minto.ai>
2024-03-21 01:31:57 +0530

    Added copilot.lua extension

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,7 +3,13 @@ return  {{
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     local builtin = require("telescope.builtin")
-    vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+    local M = {}
+    M.find_files = function()
+    builtin.find_files {
+      find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden' }
+    }
+    end
+    vim.keymap.set('n', '<leader>ff', M.find_files, {})
     vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
   end
 },
